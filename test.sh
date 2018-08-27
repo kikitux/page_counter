608fd7fbd5e9ba6b292a6271d48cbac6a5c09989
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-08-27 16:36:47 +0200

    add basic webapp

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+#test cli app
 out=$(python app.py)
 if [ "$out" > 0 ]; then
   echo Good
@@ -6,3 +8,16 @@ else
   echo Bad
   exit 1
 fi
+
+
+# start our webapp
+python webapp.py &
+# test webapp
+out2=$(curl -sL http://127.0.0.1:8080)
+if [ "$out2" > 0 ]; then
+  echo Good
+else
+  echo Bad
+  exit 1
+fi
+
